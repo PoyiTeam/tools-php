4540707352d7d63fc7164742833bff33e2bc9b1e
pyChill <pyliu.chill@gmail.com>
2023-12-28 15:54:07 +0800

    LGTM!!

--- MySQLCommand/index.php
+++ MySQLCommand/index.php
@@ -5,19 +5,53 @@ $host = "localhost:3308";
 $username = "root";
 $password = "";
 $database = "mydatabase";
-$link = mysqli_connect($host, $username, $password, $database);
 
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+// $link = mysqli_connect($host, $username, $password, $database);
+// if (!$link) {
+//     echo "Error: Unable to connect to MySQL." . PHP_EOL;
+//     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+//     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+//     exit;
+// } else {
+//     echo "Connect Success<br>";
+//     echo "Host: {$link->host_info}<br>";
+//     echo "Database: {$database}<br>";
+// }
+// mysqli_close($link);
+
+
+
+$mysql = new mysqli($host, $username, $password, $database);
+// $mysql->connect();
+
+if ($mysql->connect_errno) {
+    printf("Connect failed: %s<br />", $mysqli→connect_error);
+    exit();
 }
 
-echo "Success: A proper connection to MySQL was made! The database: {$database} is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
-mysqli_close($link);
+// create table
+$table = "myTable";
+
+$sql = "CREATE TABLE {$table} ( " .
+    "id INT NOT NULL AUTO_INCREMENT, " .
+    "title VARCHAR(100) NOT NULL, " .
+    "author VARCHAR(40) NOT NULL, " .
+    "submission_date DATE, " .
+    "PRIMARY KEY ( id )); ";
+
+var_dump($mysql->error_list);
+if ($mysql->query($sql)) {
+    echo "table created successfully<br>";
+}
+if ($mysql->errno) {
+    echo "Could not create table<br>";
+    echo "error code: {$mysql->errno}<br>";
+    echo "error message: {$mysql->error}<br>";
+}
 
 
+// $sql = "INSERT ";
+
+$mysql->close();
 ?>
